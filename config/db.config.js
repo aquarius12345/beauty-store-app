011294c9f2b2e79df582f9158dcf7004db205385
Debora Seo <deborah_seo@hotmail.com>
2021-09-29 16:57:14 -0300

    created files structure and connection to database

---
+++ config/db.config.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    console.log(`Connected to Mongo! Database name: ${connect.connections[0].name}`)
+};
+
+module.exports = connectDB;
