fe0bf9c63ec858489174da455d0fcf9e12603a21
Debora Seo <deborah_seo@hotmail.com>
2021-09-30 16:03:14 -0300

    created middleware

---
+++ middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+
+
+const auth = (req, res, next) => {
+    //verificar se recebi token
+    const token = req.get('Authorization');
+
+    if(!token) {
+        res.status(401).json({ message: 'Request without token'});
+    }
+    //validar token
+    const tokenWithoutBearer = token.split('')[1];
+
+    try {
+        const decodedToken = jwt.verify(
+            tokenWithoutBearer,
+            process.env.SECRET_JWT
+        )
+        console.log(decodedToken);
+        req.user = { ...decodedToken }
+        next();
+    } catch(error) {
+        res.status(401).json({ message: 'Unauthorized' });
+    }
+    
+};
+
+module.exports = auth;
